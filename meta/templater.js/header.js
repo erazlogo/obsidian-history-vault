97db5099e1121ac92dc3f2cb00057b7f8f9286e2
Elena Razlogova <elena.razlogova@gmail.com>
2022-06-07 00:41:00 -0400

    Add page no to research note header

--- meta/templater.js/header.js
+++ meta/templater.js/header.js
@@ -1,6 +1,9 @@
 function header(tp) {
     string=tp.file.content;
-    headertext=string.substring(0, string.indexOf("### Note")+10);
-    return headertext
+    string2=tp.file.selection();
+    headertext=string.substring(0, string.indexOf("# Annotations"));
+    pageno=string2.substring(string2.indexOf("?page=")+6, string2.indexOf("&annotation"));
+    headertext2=headertext.replace("page-no::", "page-no:: "+pageno)
+     return headertext2
 }
 module.exports = header
