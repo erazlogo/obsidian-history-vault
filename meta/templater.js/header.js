72fb38531643ac2551d53340c1c7aef772680ed7
Elena Razlogova <elena.razlogova@gmail.com>
2022-06-09 08:38:42 -0400

    Fix header.js error

--- meta/templater.js/header.js
+++ meta/templater.js/header.js
@@ -1,7 +1,7 @@
 function header(tp) {
     content=tp.file.content;
     selection=tp.file.selection();
-    getheader=content.substring(0, string.indexOf("### Note")+10);
+    getheader=content.substring(0, content.indexOf("### Note")+10);
     pageno=selection.substring(selection.indexOf("?page=")+6, selection.indexOf("&annotation"));
     headerwithpageno=getheader.replace("page-no::", "page-no:: "+pageno);
      return headerwithpageno;
